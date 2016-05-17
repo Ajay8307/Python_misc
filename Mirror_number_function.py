f0e2f23571477313ea3fbe6be1ae2a23a957fd6c
Ajay8307 <atom8307@gmail.com>
2016-05-16 22:35:15 -0700

    Update Mirror_number_function.py
    Updated if condition to account for even length string.

--- Mirror_number_function.py
+++ Mirror_number_function.py
@@ -4,9 +4,9 @@
 def mirror(num):
     """ Function to determine if a number is a mirror number or not"""
     li=list(str(num))
-    length = len(li)-1
+    length = len(li)
     for i in range(length//2):
-        if li[i]!=li[length-i]:
+        if li[i]!=li[length-i-1]:
             print("Not a mirror number")
             return
     print("Mirror number!")
